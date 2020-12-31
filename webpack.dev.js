1365446787e33efc740b04cf6aee57d7e269f102
Ekansh Singhal <ekanshsinghal@protonmail.com>
2020-12-31 14:57:42 +0530

    added support for images and fonts

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: './src/index.js',
+	entry: ["@babel/polyfill", path.resolve(__dirname, 'src/index.js')],
 	mode: 'development',
 	output: {
 		path: path.join(__dirname, '/dist'),
@@ -16,10 +16,15 @@ module.exports = {
 	module: {
 		rules: [
 			{
+				// config for loading .js and .jsx files
 				test: /\.jsx?$/,
 				exclude: /node_modules/,
-				loader: 'babel-loader'
+				use: {
+                    loader: "babel-loader",
+                    options: {presets: ["@babel/env"]}
+                },
 			}, {
+				// config for loading sass and scss files
 				test: /\.s[ac]ss$/i,
 				use: [
 					"style-loader",
@@ -27,8 +32,17 @@ module.exports = {
 					"sass-loader",
 				]
 			}, {
+				// config for loading css files
 				test: /\.css$/i,
 				use: ['style-loader', 'css-loader'],
+			}, {
+				// config for loading image files
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			}, {
+				// config for loading fonts
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource',
 			}
 		]
 	},
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: './src/index.js',
+	entry: ["@babel/polyfill", path.resolve(__dirname, 'src/index.js')],
     mode: 'production',
 	output: {
 		path: path.join(__dirname, '/dist'),
@@ -24,6 +24,12 @@ module.exports = {
 			}, {
 				test: /\.css$/i,
 				use: ['style-loader', 'css-loader'],
+			}, {
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			}, {
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource',
 			}
 		]
 	},
